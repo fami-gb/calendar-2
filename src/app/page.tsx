bfd02090305ccb2fa8022d75e7dd759b1da65db4
fami-gb <fami.gb0825@gmail.com>
2024-12-13 15:06:26 +0900

    refactor: UI改修

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -199,8 +199,8 @@ export default function Home() {
                     <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                       <div className="sm:flex sm:items-start">
                         <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center 
-                      justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                          <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                      justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                          <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                         </div>
                         <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                           <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
@@ -263,8 +263,8 @@ export default function Home() {
                     <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                       <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                         <div className="sm:flex sm:items-start">
-                          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-                            <ExclamationTriangleIcon className="h-6 w-6 text-red-600" aria-hidden="true" />
+                          <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-green-100 sm:mx-0 sm:h-10 sm:w-10">
+                            <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
                           </div>
                           <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                             <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">Edit Event</Dialog.Title>
@@ -274,9 +274,9 @@ export default function Home() {
                               shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 
                               focus:ring-2 
                               focus:ring-inset focus:ring-violet-600 
-                              sm:text-sm sm:leading-6"
-                              style={{ width: '300px', height: '50px' }}
-                                value={newEvent.title} onChange={(e) => handleChange(e)} placeholder="編集後のタイトルを入力してください" />
+                              sm:text-sm sm:leading-6
+                              w-300 h-30"
+                              onChange={(e) => handleChange(e)} placeholder="編集後のタイトルを入力してください" />
                             </div>
                           </div>
                         </div>
